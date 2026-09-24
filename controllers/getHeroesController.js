50dc4dc0b59d764efc5f4ce1bec177109697869f
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-09-24 09:11:52 -0400

    actually serving heroes, i wasnt sending them since i was using jeson incorrectly

--- controllers/getHeroesController.js
+++ controllers/getHeroesController.js
@@ -5,7 +5,7 @@ export async function getHeroes(req, res){
     try {
         const db = await getDBConnection()
         const heroes = await db.all(`SELECT * FROM heroes`)
-        res.status(200).json(heroes)
+        res.status(200).json({heroes})
     }catch (err){
         res.status(500).json({error: 'Failed to pull Heroes from database'})
     }
